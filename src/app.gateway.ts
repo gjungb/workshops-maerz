a2ac8ff53400595de83cf01180678a5e31f338e2
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-03-12 13:02:46 +0100

    Add WebSocket support Refs #12

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,11 +1,16 @@
 import {
   MessageBody,
+  OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import WebSocket from 'ws';
 
 @WebSocketGateway()
-export class AppGateway {
+export class AppGateway implements OnGatewayConnection {
+  handleConnection(client: WebSocket, ...args: any[]) {
+    client.send('connected');
+  }
   @SubscribeMessage('message')
   handleMessage(@MessageBody() data: string): string {
     return `Hallo ${data}`;
